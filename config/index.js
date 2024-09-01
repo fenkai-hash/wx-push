b45618fd0f3da1f150f4b7a571cbacf70c62e72c
fenkai-hash <3167229721@qq.com>
2024-09-01 12:10:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxe73a0d246f88ab6b",
+  appsecret: "1af91a7140b52fd9a71d1e53606de749",
+  template_id: "1af91a7140b52fd9a71d1e53606de749",
+  touser: "owtrT6iFmmevjYXIxFVW-mt6H_4o",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
+  city: "银川",
   first_date: "2022-11-10",
 };
